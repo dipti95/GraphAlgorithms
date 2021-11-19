19a56dc831ef918defc21701c961f3e2b71619fc
dipti <dipti.yadav@outlook.com>
2021-11-19 14:15:08 -0500

    River Sizes

---
+++ riverSizes.js
@@ -0,0 +1,8 @@
+// Given => 2d array(matrix)
+// find the size of all the river
+// matrix contain 0s and 1s only each 0s represent land and
+// each 1s represent part of river
+// river consist of any number of 1sthat are either horizontally
+//or vertically adjacent ( not diagonally adjacent)
+// the number of adjacent 1s forming a river;
+//Return an arrayof thesixes of all the river represented in the input given (matrix)
