aa3f701ba04cf628f8d78a8cbfe0b83716f4a748
dipti <dipti.yadav@outlook.com>
2021-11-23 07:20:30 -0500

    Populating Next Right Pointers in Each Node

--- populatingNextRightPointersInEachNode.js
+++ populatingNextRightPointersInEachNode.js
@@ -5,9 +5,28 @@
 //Initially, all next pointers are set to NULL.
 
 function connect(root) {
+  // Hear root is Tree
   if (!root) return null;
   // we set the 1st value of queue as a root node
   // it means the length of the queue is right now 1
   // but Queue has access of all the root.
+
+  //hear i did't construct the binary tree;
   let queue = [root];
+  // Now we have to  check that queue.length
+  // if queue.length will be zero we have to stop and return root (tree)
+  while (queue.length) {
+    let size = queue.length;
+
+    for (let i = 0; i < size; i++) {
+      let node = queue.shift();
+
+      if (node.left) queue.push(node.left);
+      if (node.rigth) queue.push(node.right);
+
+      if ((i = size - 1)) node.next = null;
+      else node.next = [queue[0]];
+    }
+  }
+  return root;
 }
