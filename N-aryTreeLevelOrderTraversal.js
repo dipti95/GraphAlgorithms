f824adce50c83733d9d6b7e86d0ffef77d947feb
dipti <dipti.yadav@outlook.com>
2021-11-23 21:59:56 -0500

    N-ary Tree Level Order Traversal

--- N-aryTreeLevelOrderTraversal.js
+++ N-aryTreeLevelOrderTraversal.js
@@ -2,6 +2,19 @@ function levelOrder(root) {
   if (!root) return [];
   let result = [];
   let queue = [root];
+  //level indicate the level
+  //for example
+  //       3
+  //      / \
+  //    4     6
+  //   /|\    /\
+  // 2  7  9 1  6
+
+  // In this given tree
+  // we can say that level 0 = 3
+  // level 1 = 4,6
+  // level 2 = 2,7,9,1,6
+
   let level = 0;
 
   while (queue.length) {
