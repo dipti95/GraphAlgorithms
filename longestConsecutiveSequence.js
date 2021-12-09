9da5860e667450beee8fcaa11dc192054c974be4
dipti <dipti.yadav@outlook.com>
2021-12-09 16:50:31 -0500

    longestConsecutiveSequence

---
+++ longestConsecutiveSequence.js
@@ -0,0 +1,14 @@
+function longestConsecutiveSequence(nums) {
+  if (nums.length === 0) return 0;
+  nums = nums.sort((a, b) => a - b);
+  let count = 1;
+  let currCount = 1;
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] - nums[i - 1] === 1) {
+      currCount += 1;
+      count = Math.max(count, currCount);
+    } else if (nums[i] === nums[i - 1]) continue;
+    else currCount = 1;
+  }
+  return count;
+}
